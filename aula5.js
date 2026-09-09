c2f9ae45d32f6b986cecae894f92be0988023e87
Rfaela-m <rafaelamelo@acad.ftec.com.br>
2026-09-08 21:50:34 -0300

    Add aula5.js with variable and constant examples

---
+++ aula5.js
@@ -0,0 +1,66 @@
+1
+
+// Criar uma constante
+const nome = "Rafaela";
+
+// Criar uma variável e atribuir um valor
+let idade = 26;
+
+console.log(`Olá ${nome} voce tem ${idade} anos`);
+
+2
+
+// Criar uma constante
+const produto = 150.00;
+
+
+// Constante
+const percentualDeDesconto = 15;
+
+// Porcentagem
+
+let multiplicacao = 15 * 100;
+
+let divisao = multiplicacao / 150.00;
+
+let subtracao = 150 - divisao
+
+console.log(`O valor original era R$ ${produto} e com desconto ficou R$ ${subtracao}`);
+
+
+3
+
+// Criar uma variável e atribuir um valor
+let ano = 2013;
+
+let subtracao2 = 2026 - ano;
+
+if (subtracao2 >= 18) {
+    // atingiu a maioridade
+}
+else {
+//menor de idade
+}
+
+
+4
+
+//definicao de variavel
+let entrada = "10";
+let esperado = 10;
+
+let conteudo = "nao"
+let tipoConteudo = "nao"
+
+if (entrada == esperado) {
+  conteudo = "sim"
+}
+
+if (entrada === esperado) {
+  tipoConteudo = "sim"
+}
+
+
+console.log(`as variaveis ${conteudo} sãos iguais. As variavies ${tipoConteudo} tem tipo e conteudo iguais.`);
+
+//console.log(`O operador de igualdade == avalia apenas número e o operador de igualdade estrita === avalia o texto como um topo. Desta forma, ${ediferente} é diferente de ${true}`);
